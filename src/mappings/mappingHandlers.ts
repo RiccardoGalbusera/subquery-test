441600794d05fc4d2d479ff4822177f55b1c4109
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2024-05-28 10:00:14 +0200

    add autogenerated files

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,4 @@
+import { trackEvent } from "../utils/entities";
+import { ClaimedEvent as Claimed_9dfe8ce3_cf5b_46ba_a050_993b27b3f55f} from "../types/contracts/X3f16f8964f8f5ab58929b58619056461f37f7317";
+  export async function handle9dfe8ce3_cf5b_46ba_a050_993b27b3f55f(event: Claimed_9dfe8ce3_cf5b_46ba_a050_993b27b3f55f): Promise<void> {
+    await trackEvent(event, "9dfe8ce3_cf5b_46ba_a050_993b27b3f55f", ["9dfe8ce3-cf5b-46ba-a050-993b27b3f55f"],["user","tokenId"])}
